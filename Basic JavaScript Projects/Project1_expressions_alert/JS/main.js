62533096168fe3014bb79097c09d778c58db1c89
Connor <connor.randall669@gmail.com>
2021-06-15 21:10:40 -0400

    Assignment update

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,6 @@
-document.write("Hello, World!");
+window.alert("Hello, World!"); //This provides the window alert you have to close
+var x = "This is a variable"; //This assign X to the expression "This is a variable"
+var x = x.fontcolor("green"); //This assigns the font color to X variable
+var y = " and this is the concatenating a string line of the code."; //This is declaring the Y variable
+var y = y.fontcolor("blue"); //This assigns the font color to Y variable
+document.write(x + y); //This concatenates the two strings and writes them out as one on the webpage
