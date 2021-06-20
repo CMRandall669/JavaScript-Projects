2e00749ae10f65bc47ee0bdc38910d9aaea16bc6
CMRandall669 <connor.randall669@gmail.com>
2021-06-20 09:14:26 -0400

    added logic

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,20 @@
-document.write(typeof "10" + 5);
+function NaN_function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function infinity_Positive() {
+    document.getElementById("Test").innerHTML = isNaN("This is a string");
+}
+
+function infinity_Negative() {
+    document.getElementById("Test").innerHTML = isNaN("007");
+}
+
+document.write(2e310);
+
+document.write(-3e310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
